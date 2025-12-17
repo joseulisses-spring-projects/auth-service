45e140099e8317f12f2c0db20970c2f335f86408
José Ulisses Fernandes Junior <11252401443@alunos.umc.br>
2025-12-17 14:58:12 -0300

    feat: criar entidade User

---
+++ auth-service/src/main/java/br/com/joseulisses/authservice/domain/entity/User.java
@@ -0,0 +1,32 @@
+package br.com.joseulisses.authservice.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean enabled = true;
+}

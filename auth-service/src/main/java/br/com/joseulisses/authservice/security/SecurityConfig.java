2ef7e679420b70666edfaa347fe8ee966b40ca73
José Ulisses Fernandes Junior <11252401443@alunos.umc.br>
2025-12-22 09:59:42 -0300

    feat: configurar Spring Security para API REST

---
+++ auth-service/src/main/java/br/com/joseulisses/authservice/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package br.com.joseulisses.authservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable());
+        return http.build();
+    }
+}

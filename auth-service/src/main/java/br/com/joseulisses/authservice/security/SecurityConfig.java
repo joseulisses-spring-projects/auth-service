57896d5867ea4edbc6f7ed56e65d80b0dba07b30
José Ulisses Fernandes Junior <11252401443@alunos.umc.br>
2025-12-26 14:53:32 -0300

    feat: implementa autenticação com JWT

--- auth-service/src/main/java/br/com/joseulisses/authservice/security/SecurityConfig.java
+++ auth-service/src/main/java/br/com/joseulisses/authservice/security/SecurityConfig.java
@@ -2,26 +2,36 @@ package br.com.joseulisses.authservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users", "/users/**", "/error").permitAll()
+                        .requestMatchers(
+                                "/auth/login",
+                                "/users"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form.disable())
-                .httpBasic(basic -> basic.disable());
+                //  REGISTRA O FILTRO JWT
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
